0fcd2c1b192698240e9860ec86d08e868ba81601
p11 <maisonatelier52@gmail.com>
2026-05-22 17:43:46 +0530

    robots.txt changed

--- app/robots.js
+++ app/robots.js
@@ -1,17 +1,34 @@
+// // app/robots.js
+
+// export default function robots() {
+//   return {
+//     rules: [
+//       {
+//         userAgent: "*",           // Applies to all crawlers (Google, Bing, etc.)
+//         allow: "/",               // Allow full access to the site
+//         disallow: ["/_next/"], // Block Next.js internal paths
+//         // If you want to block specific paths in the future, uncomment and customize:
+//         // disallow: ["/admin/", "/private/", "/preview/"],
+//       },
+//     ],
+//     sitemap: "https://www.courtnews.org/sitemap.xml",   // Link to your sitemap
+//     host: "https://www.courtnews.org",                  // Canonical domain
+//   };
+// }
+
 // app/robots.js
 
 export default function robots() {
   return {
     rules: [
       {
-        userAgent: "*",           // Applies to all crawlers (Google, Bing, etc.)
-        allow: "/",               // Allow full access to the site
-        disallow: ["/_next/"], // Block Next.js internal paths
-        // If you want to block specific paths in the future, uncomment and customize:
-        // disallow: ["/admin/", "/private/", "/preview/"],
+        userAgent: "*",
+        allow: ["/", "/_next/static/"],
+        disallow: ["/_next/"],
       },
     ],
-    sitemap: "https://www.courtnews.org/sitemap.xml",   // Link to your sitemap
-    host: "https://www.courtnews.org",                  // Canonical domain
+    sitemap: "https://www.courtnews.org/sitemap.xml",
+    host: "https://www.courtnews.org",
   };
 }
+
